9575a3213d86ea8a8d4ddc8937bf6ac2d0969ad2
Mikko Haapoja <me@mikkoh.com>
2015-02-22 00:42:32 -0500

    Implemented some animations

--- frontend/lib/docs.js
+++ frontend/lib/docs.js
@@ -1,9 +1,9 @@
 var vue = require( 'vue' );
+var Tween = require( 'gsap' );
 var xhrJson = require( 'xhr-json' );
 var find = require( 'dom-select' );
 var remove = require( 'dom-remove' );
 var model = require( './model' );
-
 
 module.exports = docs;
 
@@ -20,26 +20,54 @@ docs.prototype = {
 
       if( data.ok ) {
 
+        var containerDoc = find( '#docs-container' );
+        var container = this.container = document.createElement( 'div' );
+
+        container.className = 'container';
+
+        containerDoc.appendChild( container );
+
         var docVue = this.docVue = new vue( {
 
-          el: '#docs-container',
+          el: container,
           template: '#docs-template',
           data: data,
           attached: function() {
 
             var allImageTags = find.all( 'img', this.$el );
+            var numLoaded = 0;
             var img;
 
-            for( var i = 0, len = allImageTags.length; i < len; i++ ) {
+            var imgFinish = function() {
 
-              img = allImageTags[ i ];
-              img.onerror = function() {
+              if( ++numLoaded == allImageTags.length ) {
 
-                remove( this );
-              }.bind( img );
+                done();
+              }
+            };
+
+            if( allImageTags.length > 0 ) {
+
+              for( var i = 0, len = allImageTags.length; i < len; i++ ) {
+
+                img = allImageTags[ i ];
+
+                img.onload = function() {
+
+                  imgFinish();
+                };
+
+                img.onerror = function() {
+
+                  remove( this );
+
+                  imgFinish();
+                }.bind( img );
+              }  
+            } else {
+
+              done();
             }
-
-            done();
           }
         });
       } else {
@@ -49,11 +77,21 @@ docs.prototype = {
     }.bind( this ));
   },
 
+  aniIn: function( req, done ) {
+
+    Tween.to( this.container, 0.25, { delay: 0.125, opacity: 1, ease: Expo.easeOut, onComplete: done } );
+  },
+
+  aniOut: function( req, done ) {
+
+    Tween.to( this.container, 0.25, { opacity: 0, ease: Expo.easeOut, onComplete: done } );
+  },
+
   destroy: function( req, done ) {
 
-    console.log( 'destroyed' );
-    
     this.docVue.$destroy( true );
+
+    remove( this.container );
 
     done();
   }
--- frontend/lib/menu.js
+++ frontend/lib/menu.js
@@ -1,82 +1,141 @@
 var vue = require( 'vue' );
+var find = require( 'dom-select' );
+var remove = require( 'dom-remove' );
+var Tween = require( 'gsap' );
 var model = require( './model' );
 var framework = require( './framework' );
 
-var path, moduleName, treePart;
+module.exports = menu;
 
-var menu = new vue( {
+var prevPath;
 
-  el: '#menu-container',
-  template: '#menu-template',
-  data: model,
-  methods: {
+function menu() {}
 
-    onClick: function( e ) {
-
-      path.push( e.target.innerText );
-
-      setupPath();
-    }
-  }
-});
-
-var get = function( data, path ) {
-
-  var returnValue = data;
-
-  path.forEach( function( part ) {
-
-    returnValue = returnValue[ part ];
-  });
-
-  return returnValue;
-};
-
-var getTreePart = function() {
-
-  if( path.length > 0 ) {
-
-    return get( model.menu.tree.modules, path );  
-  } else {
-
-    return model.menu.tree.modules;
-  }
-};
-
-var updateVueData = function() {
-
-  menu.$data = {
-
-    name: moduleName,
-    modules: Object.keys( treePart ).length > 0 ? treePart : null
-  }
-};
-
-var setupPath = function() {
-
-  treePart = getTreePart();
-
-  framework.go( path.join( '/' ) );
-};
-
-module.exports = {
+menu.prototype = {
 
   init: function( req, done ) {
 
-    if( req.splats[ 0 ] != '' ) {
+    var path, moduleName, treePart;
 
-      path = req.splats[ 0 ].split( '/' );  
-      moduleName = path[ path.length - 1 ];
+    var containerMenu = find( '#menu-container' );
+    var container = this.container = document.createElement( 'div' );
+
+    container.className = 'container';
+
+    containerMenu.appendChild( container );
+
+    var menu = this.menuVue = new vue( {
+
+      el: container,
+      template: '#menu-template',
+      data: model,
+      methods: {
+
+        onClick: function( e ) {
+
+          framework.go( path.join( '/' ) + '/' + e.target.innerText );
+        }
+      }
+    });
+
+    var get = function( data, path ) {
+
+      var returnValue = data;
+
+      path.forEach( function( part ) {
+
+        returnValue = returnValue[ part ];
+      });
+
+      return returnValue;
+    };
+
+    var getTreePart = function() {
+
+      if( path.length > 0 ) {
+
+        return get( model.menu.tree.modules, path );  
+      } else {
+
+        return model.menu.tree.modules;
+      }
+    };
+
+    var updateVueData = function() {
+
+      menu.$data = {
+
+        name: moduleName,
+        modules: Object.keys( treePart ).length > 0 ? treePart : null
+      }
+    };
+
+    this.path = path = getPath( req );
+
+    if( path.length > 0 ) {
+
+      moduleName = path[ path.length - 1 ];  
     } else {
 
-      path = [];
-      moduleName = model.menu.tree.moduleName;
+      moduleName = model.menu.tree.moduleName;  
     }
-
+    
     treePart = getTreePart();
 
     updateVueData();
 
     done();
+  },
+
+  aniIn: function( req, done ) {
+
+    if( !prevPath || this.path.length > prevPath.length ) {
+
+      this.container.style.left = '200px';
+
+      Tween.to( this.container, 0.5, { left: 0, opacity: 1, ease: Expo.easeOut, onComplete: done } );  
+    } else {
+
+      this.container.style.left = '-200px';
+
+      Tween.to( this.container, 0.5, { left: 0, opacity: 1, ease: Expo.easeOut, onComplete: done } );  
+    }
+    
+    prevPath = this.path.concat();
+  },
+
+  aniOut: function( req, done ) {
+
+    if( this.path.length < getPath( req ).length ) {
+
+      Tween.to( this.container, 0.5, { left: -200, opacity: 0, ease: Expo.easeOut, onComplete: done } );  
+    } else {
+
+      Tween.to( this.container, 0.5, { left: 200, opacity: 0, ease: Expo.easeOut, onComplete: done } );
+    }
+  },
+
+  destroy: function( req, done ) {
+
+    this.menuVue.$destroy( true );
+
+    remove( this.container );
+
+    done();
   }
 };
+
+function getPath( req ) {
+
+  var path;
+
+  if( req.splats[ 0 ] != '' ) {
+
+    path = req.splats[ 0 ].split( '/' );  
+  } else {
+
+    path = [];
+  }
+
+  return path;
+}
